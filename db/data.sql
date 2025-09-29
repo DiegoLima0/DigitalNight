435695cac51e9aa8cf3468868d5bf89e2019afe5
DiegoLima0 <diegoalanlima98@gmail.com>
2025-09-29 15:30:22 -0300

    reorganizacion de base de datos

--- db/data.sql
+++ db/data.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-09-2025 a las 01:29:58
+-- Tiempo de generación: 29-09-2025 a las 20:21:05
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -24,6 +24,38 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `creator`
+--
+
+CREATE TABLE `creator` (
+  `creatorName` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `correo` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `idCreator` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `game`
+--
+
+CREATE TABLE `game` (
+  `tittle` varchar(255) DEFAULT NULL,
+  `genre` varchar(255) DEFAULT NULL,
+  `state` varchar(255) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `imagen` longblob DEFAULT NULL,
+  `game` longblob DEFAULT NULL,
+  `idCreator` int(11) NOT NULL,
+  `idGame` int(11) NOT NULL,
+  `verified` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `register`
 --
 
@@ -31,12 +63,25 @@ CREATE TABLE `register` (
   `username` varchar(255) NOT NULL,
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
-  `id` int(11) NOT NULL
+  `id` int(11) NOT NULL,
+  `type` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `creator`
+--
+ALTER TABLE `creator`
+  ADD PRIMARY KEY (`idCreator`);
+
+--
+-- Indices de la tabla `game`
+--
+ALTER TABLE `game`
+  ADD PRIMARY KEY (`idCreator`,`idGame`);
 
 --
 -- Indices de la tabla `register`
@@ -54,6 +99,22 @@ ALTER TABLE `register`
 --
 ALTER TABLE `register`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `creator`
+--
+ALTER TABLE `creator`
+  ADD CONSTRAINT `fk_Creator_User` FOREIGN KEY (`idCreator`) REFERENCES `register` (`id`);
+
+--
+-- Filtros para la tabla `game`
+--
+ALTER TABLE `game`
+  ADD CONSTRAINT `fk_Game_Creator` FOREIGN KEY (`idCreator`) REFERENCES `creator` (`idCreator`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
