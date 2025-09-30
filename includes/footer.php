2a605ff03b44cacebc30fbffd0bee34ee654a6fa
DiegoLima0 <diegoalanlima98@gmail.com>
2025-09-30 13:28:32 -0300

    inclucion de footer y nav

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+<html>
+    <footer>
+        <img src="img/Digital Night logo blanco letras.png" alt="Logo Digital Night">
+       
+        <div>
+            <a href="">Sobre nosotros</a>
+            <a href="">Soporte</a>
+        </div>
+        <hr>
+        <p>Penta-core</p>
+    </footer>
+</html>
---
+++ includes/navbar.php
@@ -0,0 +1,8 @@
+<html>
+    <nav>
+        <a href="">Tienda</a>
+        <a href="">Biblioteca</a>
+        <a href="">Sobre nosotros</a>
+        <a href="">Soporte</a>
+    </nav>
+</html>
