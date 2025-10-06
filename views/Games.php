6ca88f3b073b6fa0d09dfd18e8625f59ff335abe
Facundomontes <facundomontes969@gmail.com>
2025-10-05 21:53:32 -0300

    Subo cambios games

--- views/Games.php
+++ views/Games.php
@@ -11,55 +11,69 @@
 <body>
     <?php include("../includes/header.php"); ?>
 
-    <section class="game-hero">
-        <div class="game-cover"></div>
-        <div class="game-info">
-            <h1>Red dead redemption 2</h1>
-            <p>Red dead redemption 2:Con más de 175 premios al Juego del año y más de 250 valoraciones perfectas, Red Dead Redemption 2 es la épica historia de Arthur Morgan y la banda de Van der Linde, que huyen por toda América en el albor de una nueva era. También incluye acceso al mundo multijugador compartido de Red Dead Online.</p>
-        </div>
-    </section>
+    <div class="page-grid">
+       
+        <section class="banner-grid">
+            <div class="game-banner">
+                <div class="game-cover">
+                    <img src="../img/rdr2.jpg" alt="Nombre del juego 3">
+                </div>
+                <div class="game-info">
+                    <h1>Red dead redemption 2</h1>
+                    <p class="descripcion">América, 1899. Arthur Morgan y la banda de Van der Linde son forajidos en busca y captura. Mientras los agentes federales y los mejores cazarrecompensas de la nación les pisan los talones, la banda deberá abrirse camino por el abrupto territorio del corazón de América y sobrevivir a base de robos y peleas. Mientras las divisiones internas aumentan y amenazan con separarlos a todos, Arthur deberá elegir entre sus propios ideales y la lealtad a la banda que lo vio crecer.
 
-    <section class="game-gallery">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-    </section>
+Con más de 175 premios al Juego del año y más de 250 valoraciones perfectas, Red Dead Redemption 2 es una historia épica sobre el honor y la lealtad en el umbral de una nueva era.
 
-    <section class="content">
-        <aside class="sidebar">
-            <h3>Información</h3>
-            <p><b>Desarrollador:</b> Nombre</p>
-            <p><b>Fecha de lanzamiento:</b> 4 Sep 2025</p>
-            <p><b>Género:</b> Aventura</p>
-        </aside>
-
-        <main class="main">
-            <div class="comment">
-                <h4>@Usuario</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget turpis ultricies mauris.</p>
+Red Dead Redemption 2 también incluye Red Dead Online, la experiencia multijugador basada en el mundo vivo de Red Dead Redemption 2. Cabalga en solitario o forma una cuadrilla; vende licor; enfréntate a agentes de la ley, bandas de forajidos, feroces animales salvajes y mucho más mientras forjas tu destino en el Oeste</p>
+                </div>
             </div>
-            <div class="comment">
-                <h4>@Usuario</h4>
-                <p>Comentario con más detalle sobre el juego y sus características.</p>
-            </div>
-            <div class="comment">
-                <h4>@Usuario</h4>
-                <p>Otro comentario sobre el juego. Suspendisse eget turpis ultricies mauris.</p>
-            </div>
-        </main>
+        </section>
 
-        <aside class="recommendations">
-            <h3>Recomendados</h3>
-            <div class="recommendation-item">Juego 1</div>
-            <div class="recommendation-item">Juego 2</div>
-            <div class="recommendation-item">Juego 3</div>
-        </aside>
-    </section>
+        <section class="content-grid">
+            <section class="game-gallery">
+                <div> <img src="../img/foto games 1.jpg" alt="Nombre del juego 3"></div>
+                <div><img src="../img/foto games 2.jpg" alt="Nombre del juego 3"></div>
+                <div><img src="../img/foto games 3.jpg" alt="Nombre del juego 3"></div>
+                <div><img src="../img/foto games 5.jpg" alt="Nombre del juego 3"></div>
+                <div><img src="../img/foto games 4.jpg" alt="Nombre del juego 3"></div>
+            </section>
 
-    <?php include("includes/footer.php"); ?>
+            <section class="content">
+                <aside class="sidebar">
+                    <h3>Información</h3>
+                    <p><b>Desarrollador:</b> Rockstar games</p>
+                    <p><b>Fecha de lanzamiento:</b> 2018</p>
+                    <p><b>Género:</b> Mundo abierto</p>
+                </aside>
 
+                <main class="main">
+                    <div class="comment">
+                        <h4>@Usuario</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget turpis ultricies mauris.</p>
+                    </div>
+                    <div class="comment">
+                        <h4>@Usuario</h4>
+                        <p>Comentario con más detalle sobre el juego y sus características.</p>
+                    </div>
+                    <div class="comment">
+                        <h4>@Usuario</h4>
+                        <p>Otro comentario sobre el juego. Suspendisse eget turpis ultricies mauris.</p>
+                    </div>
+                </main>
+
+                <aside class="recommendations">
+                    <h3>Recomendados</h3>
+                    <div class="recommendation-item">Juego 1</div>
+                    <div class="recommendation-item">Juego 2</div>
+                    <div class="recommendation-item">Juego 3</div>
+                </aside>
+            </section>
+        </section>
+    </div>
+
+    <?php include("../includes/footer.php"); ?>
 </body>
+
+
 
 </html>
