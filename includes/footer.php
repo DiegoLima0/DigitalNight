be9a5daecc7298c9897044c9e61a9b03d673b635
Facundomontes <facundomontes969@gmail.com>
2025-10-14 15:07:44 -0300

    Subo cambios en php, css y img

--- includes/footer.php
+++ includes/footer.php
@@ -3,10 +3,10 @@
         <img src="../img/Digital Night logo blanco letras.png" alt="Logo Digital Night">
        
         <div>
-            <a href="">Sobre nosotros</a>
+            <a href="aboutUs.php">Sobre nosotros</a>
             <a href="support.php">Soporte</a>
             <a href="../logout.php">Cerrar Sesión</a>
-            <a href="../views/configPublicProfile.php" class="linkFooter">Editar perfil publico</a>
+            <a href="configPublicProfile.php" class="linkFooter">Editar perfil publico</a>
         <hr>
         <p>Penta-core</p>
     </footer>
