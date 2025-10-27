3740944b57d5cb1b0592d150eab60b0e5a5efda3
Facundomontes <facundomontes969@gmail.com>
2025-10-27 14:36:39 -0300

    Subo cambios

--- views/Games_view.php
+++ views/Games_view.php
@@ -11,12 +11,12 @@
   <div class="page">
 
     <div class="section fondo-img" role="banner" aria-label="Fondo principal">
-      <img src="img/red-dead-2-baner2.png" alt="" aria-hidden="true" />
+      <img src="#" alt="" aria-hidden="true" />
 
       <div class="contenedor">
         <div class="left">
-          <h1 class="title">Red dead redemption 2</h1>
-          <p class="subtitle">Revive la leyenda con gráficos modernos, jugabilidad mejorada</p>
+          <h1 class="title">Titulo del juego</h1>
+          <p class="subtitle">Muy breve descripcion del juego (dos renlones) </p>
 
           <div class="cta-group">
             <a class="boton-base boton-primario" href="#">Comprar</a>
@@ -26,9 +26,9 @@
 
         <div class="right">
           <div class="meta">
-            <div><strong>Fecha:</strong> 26 de octubre 2018</div>
+            <div><strong>Fecha:</strong> Fecha lanzamiento</div>
 
-            <div style="margin-top:.6rem"><strong>Plataformas:</strong> PS5 • PS4 • PC</div>
+            <div style="margin-top:.6rem"><strong>Plataformas:</strong> Plataformas disponibles</div>
           </div>
         </div>
       </div>
@@ -41,10 +41,10 @@
         <div class="carrusel-wrapper">
 
           <div class="carrusel" role="region" aria-label="Galería de imágenes compacta" tabindex="0">
-            <img src="img/foto-games-1.jpg" alt="Imagen 1">
-            <img src="img/foto-games-2.jpg" alt="Imagen 2">
-            <img src="img/foto-games-3.jpg" alt="Imagen 3">
-            <img src="img/foto games 4.jpg" alt="Imagen 4">
+            <img src="#" alt="Imagen 1">
+            <img src="#" alt="Imagen 2">
+            <img src="#" alt="Imagen 3">
+            <img src="#" alt="Imagen 4">
           </div>
         </div>
       </div>
@@ -52,11 +52,11 @@
 
 
     <section class="section cover-img" aria-label="Segunda imagen">
-      <img src="img/rdr2.banner3.jpg" alt="Imagen destacada del juego" />
+      <img src="#" alt="Imagen destacada del juego" />
 
       <div class="content">
-        <h2>Descripcion del juego</h2>
-        <p> Estados Unidos, 1899. El fin de la era del salvaje oeste ha comenzado. La ley da cacería a las últimas bandas de forajidos que quedan. Los que no se rinden o sucumben, terminan muertos. Después de que un robo termina mal en el pueblo de Blackwater, Arthur Morgan y la banda Van der Linde se ven obligados a huir. Con agentes federales y los mejores cazarrecompensas de la nación pisándoles los talones, la banda debe robar y pelear, abriéndose camino por el inhóspito corazón de Estados Unidos para poder sobrevivir
+        <h2>Titulo descripcion del juego</h2>
+        <p> Descripcion del juego
         </p>
       </div>
     </section>
@@ -96,38 +96,38 @@
             <article class="post-card">
               <div class="post-h">
                 <div class="avatar" aria-hidden="true">
-                  <img class="avatar" src="img/rockstar-logo.jpg" alt="Avatar de @Usuario creador" width="42" height="42" loading="lazy">
+                  <img class="avatar" src="#" alt="Avatar de @Usuario creador" width="42" height="42" loading="lazy">
                 </div>
 
                 <div class="meta">
-                  <div class="user">Rockstar Games</div>
-                  <div class="time">Hace 2h</div>
+                  <div class="user">Nombre del usuario que realizo el post</div>
+                  <div class="time">Hace cuanto lo realizo</div>
                 </div>
               </div>
 
-              <p class="post-text">Relatos curiosos del Oeste requerirá una voluntad firme y coraje para enfrentarte a lo insólito, lo retorcido y lo francamente sobrenatural. Embárcate en estas cuatro nuevas misiones por telegrama recogiendo la carta del Sr. Levin en cualquier oficina de correos o en la caja fuerte de tu campamento, y ábrela desde tu zurrón o inicia las misiones por telegrama desde el menú del jugador.</p>
+              <p class="post-text">Parrafo de la publicacion </p>
 
               <div class="post-image">
-                <img src="img/reddead-post.png" alt="imagen post 1">
+                <img src="#" alt="imagen post 1">
               </div>
             </article>
 
             <article class="post-card">
               <div class="post-h">
                 <div class="avatar" aria-hidden="true">
-                  <img class="avatar" src="img/rockstar-logo.jpg" alt="Avatar de @Usuario creador" width="42" height="42" loading="lazy">
+                  <img class="avatar" src="#" alt="Avatar de @Usuario creador" width="42" height="42" loading="lazy">
                 </div>
 
                 <div class="meta">
-                  <div class="user">Rockstar Games</div>
-                  <div class="time">Hace 1d</div>
+                  <div class="user">Nombre del usuario que realizo el post</div>
+                  <div class="time">Hace cuanto lo realizo</div>
                 </div>
               </div>
 
-              <p class="post-text"> Abastece la frontera con pieles de primera calidad y mucho más, todo ello por un precio justo. Da salida a tu inventario y haz que el trabajo duro se traduzca en cuantiosas recompensas: llévate el triple de RDO$, EXP y EXP de rol en las ventas de Comerciante. No pierdas la ambición: completa al menos cinco ventas para ganar un par de botas gratis.</p>
+              <p class="post-text"> Parrafo de la publicacion</p>
 
               <div class="post-image">
-                <img src="img/post2reddead.jpeg" alt="imagen post 2">
+                <img src="#" alt="imagen post 2">
               </div>
             </article>
 
@@ -142,34 +142,34 @@
 
             <div class="rec-item">
               <a class="rec-thumb" href="#">
-                <img src="img/gtav.jpg" alt="rec 1">
+                <img src="#" alt="rec 1">
               </a>
 
               <div class="rec-meta">
-                <div class="rec-title">Grand Theft Auto V </div>
-                <div class="rec-sub">Desarrollador: Rockstar Games</div>
+                <div class="rec-title">Titulo del juego recomendado </div>
+                <div class="rec-sub">Desarrollador del juego</div>
               </div>
             </div>
 
             <div class="rec-item">
               <a class="rec-thumb" href="#">
-                <img src="img/mafia2.jpg" alt="rec 2">
+                <img src="#" alt="rec 2">
               </a>
 
               <div class="rec-meta">
-                <div class="rec-title">Mafia 2</div>
-                <div class="rec-sub">Desarrollador: 2Kgames</div>
+                <div class="rec-title">Titulo del juego recomendado</div>
+                <div class="rec-sub">Desarrollador del juego</div>
               </div>
             </div>
 
             <div class="rec-item">
               <a class="rec-thumb" href="#">
-                <img src="img/fallou4-recomendado.jpg" alt="rec 3">
+                <img src="#" alt="rec 3">
               </a>
 
               <div class="rec-meta">
-                <div class="rec-title">Fallout 4</div>
-                <div class="rec-sub">Desarrollador: Bethesta</div>
+                <div class="rec-title">Titulo del juego recomendado</div>
+                <div class="rec-sub">Desarrollador del juego</div>
               </div>
             </div>
           </aside>
