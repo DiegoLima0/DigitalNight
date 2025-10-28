d9284c353174362a7b280296e1bdcb30a9b391e0
AmyDelgado08 <amydelgado88@gmail.com>
2025-10-28 11:26:45 -0300

    hice la tarea d Lu

--- db/data.sql
+++ db/data.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-10-2025 a las 21:42:11
+-- Tiempo de generación: 28-10-2025 a las 15:22:35
 -- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Versión de PHP: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,6 +33,13 @@ CREATE TABLE `creator` (
   `correo` varchar(255) DEFAULT NULL,
   `idCreator` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `creator`
+--
+
+INSERT INTO `creator` (`creatorName`, `country`, `correo`, `idCreator`) VALUES
+('Estudio Ejemplo', 'Desconocido', 'contacto@ejemplo.com', 1);
 
 -- --------------------------------------------------------
 
@@ -62,6 +69,16 @@ CREATE TABLE `game` (
   `gameGallery5` varchar(255) DEFAULT NULL,
   `gameGallery6` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `game`
+--
+
+INSERT INTO `game` (`title`, `genre`, `state`, `description`, `imagen`, `game`, `idCreator`, `idGame`, `verified`, `releaseDate`, `platforms`, `promoText`, `price`, `imagen2`, `gameGallery1`, `gameGallery2`, `gameGallery3`, `gameGallery4`, `gameGallery5`, `gameGallery6`) VALUES
+('Red Dead Redemption 2', 'Acción/Aventura', 'Disponible', 'Estados Unidos, 1899. El fin de la era del salvaje oeste ha comenzado. La ley da cacería a las últimas bandas de forajidos que quedan. Los que no se rinden o sucumben, terminan muertos.\n\nDespués de que un robo termina mal en el pueblo de Blackwater, Arthur Morgan y la banda Van der Linde se ven obligados a huir. Con agentes federales y los mejores cazarrecompensas de la nación pisándoles los talones, la banda debe robar y pelear, abriéndose camino por el inhóspito corazón de Estados Unidos para poder sobrevivir.\n\nA medida que las cada vez más profundas divisiones internas amenazan con separar a la banda, Arthur debe elegir entre sus propios ideales y su lealtad a la banda que lo vio crecer. \n\nDe los creadores de Grand Theft Auto V y Red Dead Redemption, Red Dead Redemption 2 es la extensa historia de la vida en Estados Unidos a principios de la era moderna.', 'rdr2.png', 0x66616c6c6f7574345f6578652e7a6970, 1, 9, 1, '2015-11-10', 'Windows, PlayStation 4, Xbox One', '59.99', '59.99', 'rdr2-2.jpg', 'rdr2-img1.jpg', 'rdr2-img2.jpg', 'rdr2-img3.jpg', 'rdr2-img4.jpg', NULL, NULL),
+('Metal Gear Solid Delta: Snake Eater', 'Acción', 'Disponible', 'METAL GEAR SOLID Δ: SNAKE EATER te lleva al comienzo de la saga de Metal Gear y cuenta la emocionante e impredecible historia que dio origen al legendario soldado Big Boss.\n\nCombina sigilo, tecnología de camuflaje, habilidades de supervivencia y dominio del combate cuerpo a cuerpo (CQC) para infiltrarte entre los grupos enemigos y evitar que las armas ultrasecretas desencadenen un conflicto nuclear a gran escala.\n\nDesarrollado íntegramente en Unreal Engine 5, el remake muestra los entornos naturales y los personajes icónicos del juego original con un nuevo nivel de detalle increíble. Un nuevo estilo de control moderno opcional ofrece una experiencia de sigilo y supervivencia más intuitiva y contemporánea, adecuada para los jugadores que experimentan el mundo de Tactical Espionage Action por primera vez.', 'mgs3.jpg', 0x7363686564756c655f6578652e7a6970, 1, 10, 1, '2024-01-01', 'PlayStation 5, Xbox Series X|S, Microsoft Windows', '69.99', '69.99', 'fondo-MetalGear.jpg', 'MetalGear-img1.jpg', 'MetalGear-img2.jpg', 'MetalGear-img3.jpg', 'MetalGear-img4.jpg', NULL, NULL),
+('Fallout 4', 'Rol / RPG', 'Disponible', 'Bethesda Game Studios, el galardonado creador de Skyrim y Fallout 76,  te desafía a explorar los vestigios de Boston y de toda la Commonwealth en este RPG de acción de mundo abierto.\n\nConviértete en el superviviente que quieres ser. Juega en el papel del único sobreviviente del Refugio 111 y forja alianzas o elige la soledad en una tierra devastada por la guerra nuclear, donde cada acción que tomes tendrá consecuencias. Solo tú puedes reconstruir y determinar el destino de esta tierra desolada.', 'fallout4r.jpg', 0x66616c6c6f7574345f6578652e7a6970, 1, 11, 1, '2015-11-10', 'Windows, PlayStation 4, Xbox One', '19.99', '19.99', 'fondo-MetalGear.jpg', 'Fallout4-img1.jpg', 'Fallout4-img2.jpg', 'Fallout4-img3.jpg', 'Fallout4-img4.jpg', NULL, NULL),
+('Schedule I', 'Estrategia', 'Disponible', 'SCHEDULE 1 te pone en la piel de un distribuidor que empieza desde lo más bajo, en las calles húmedas y decadentes de Hyland Point, una ciudad corroída por el crimen, la corrupción y la ambición. Tu objetivo es simple, pero peligroso: ascender desde vendedor callejero hasta convertirte en el rey del narcotráfico urbano.\n\nConstruí tu imperio desde cero: fabrica tus propias sustancias, controlá la producción, organizá tus rutas de distribución y expandí tus operaciones por los distintos distritos de la ciudad. Comprá propiedades, almacenes, laboratorios y negocios de fachada, contrata empleados leales —o reemplazalos si te traicionan—, y enfrenta a bandas rivales, autoridades corruptas y competidores que harán lo que sea para eliminarte.\n\nCada decisión que tomes puede hacerte más rico o hundirte en la ruina. Elegí entre mantener un perfil bajo o gobernar con violencia y miedo. Invertí en expansión, sobornos o mejoras tecnológicas para aumentar tu control sobre Hyland Point.\n\nDesarrollado con un enfoque en la gestión, el realismo urbano y la narrativa inmersiva, Schedule 1 combina estrategia, simulación y supervivencia en un entorno vivo y cambiante, donde cada acción tiene consecuencias. ¿Tenés lo necesario para construir un imperio… o vas a caer intentando dominar la ciudad?', 'Schedule1-Banner.jpg', 0x7363686564756c655f6578652e7a6970, 1, 12, 1, '2024-01-01', 'Windows', '10.49', '10.49', 'Schedule-img2-Banner.jpg', 'Schedule-img1.jpg', 'Schedule-img2.jpg', 'Schedule-img3.jpg', 'Schedule-img4.jpg', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -122,7 +139,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT de la tabla `game`
 --
 ALTER TABLE `game`
-  MODIFY `idGame` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idGame` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `user`
