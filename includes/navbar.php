dc3842830fd52e5415eb8ada0e3007f6efd3da3a
TamaraBritezClaudino <tamarabritezc@gmail.com>
2025-11-01 02:25:03 -0300

    subo cambios en html y css

--- includes/navbar.php
+++ includes/navbar.php
@@ -8,15 +8,14 @@ $is_logged_in = isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true
 
 if ($is_logged_in) {
     $clase_botones_auth = 'borrar';
-    $clase_perfil = ''; 
-    
+    $clase_perfil = '';
+
     $username = htmlspecialchars($_SESSION['userName'] ?? 'usuario');
     $profile_pic = htmlspecialchars($_SESSION['profile_picture'] ?? 'default.png');
-}
-else {
+} else {
     $clase_botones_auth = 'btn azul';
-    $clase_perfil = 'borrar'; 
-    
+    $clase_perfil = 'borrar';
+
     $username = '';
     $profile_pic = 'default.png';
 }
@@ -32,22 +31,47 @@ else {
         <a href="register.php" class="<?php echo $clase_botones_auth; ?>">
             Registrarse
         </a>
-        
-        <div class="<?php echo $clase_perfil; ?>" id="perfilMenu">
-            <p>@<?php echo $username; ?></p>
-            
-           <div class="profile-dropdown">
-  <a href="profile.php">
-    <img src="img/profiles/<?php echo $profile_pic; ?>" alt="Imagen de perfil">
-  </a>
 
-  <div class="dropdown-menu">
-    <a href="profile.php">Ver perfil</a>
-    <a href="#">⚙️Configuración</a>
-    <a href="#"> +Quiero ser creador</a>
-    <a href="logout.php"> ⮌Cerrar sesión</a>
-  </div>
-</div>
+        <div class="<?php echo $clase_perfil; ?>" id="perfilMenu">
+
+            <div class="profile-dropdown">
+
+                <i class="bi bi-person"></i>
+
+                <div class="dropdown-menu">
+                    <div>
+                        <img src="img/profiles/<?php echo $profile_pic; ?>" alt="Imagen de perfil">
+                        <p>@<?php echo $username; ?></p>
+                    </div>
+
+                    <a href="profile.php">
+                        <i class="bi bi-person"></i>
+                        Ver perfil
+                    </a>
+
+                    <a href="#">
+                        <i class="bi bi-gear"></i>
+                        Configuración
+                    </a>
+
+                    <a href="#">
+                        <i class="bi bi-person-plus"></i>
+                        Quiero ser creador
+                    </a>
+
+                    <a href="logout.php">
+                        <i class="bi bi-box-arrow-left"></i>
+                        Cerrar sesión
+                    </a>
+                </div>
+            </div>
+
+            <div>
+                <i class="bi bi-cart"></i>
+
+                <div id="numero"></div>
+            </div>
+            
         </div>
     </div>
 </div>
--- views/Games_view.php
+++ views/Games_view.php
@@ -10,7 +10,7 @@
 <body class="games">
   <div class="page">
     <div class="section fondo-img" role="banner" aria-label="Fondo principal">
-      <img src="img/<?php echo $game_data['banner_path']; ?>" alt="Banner principal del juego <?php echo $game_data['title']; ?>" aria-hidden="true" /> 
+      <img src="img/<?php echo $game_data['banner_path']; ?>" alt="Banner principal del juego <?php echo $game_data['title']; ?>" aria-hidden="true" />
 
       <div class="contenedor">
         <div class="left">
@@ -19,9 +19,10 @@
 
           <div class="cta-group">
             <form method="POST" action="buy.php">
-            <input type="hidden" name="idGame" value="<?php echo $game_data['idGame']; ?>">
-            <button type="submit" class="boton-base boton-primario">Comprar</button>
-            <a class="boton-base boton-secundario" href="#">Ver ediciones</a>
+              <input type="hidden" name="idGame" value="<?php echo $game_data['idGame']; ?>">
+              <button type="submit" class="boton-base boton-primario">Comprar ahora</button>
+              <button type="submit" class="boton-base boton-primario añadirCarrito">Añadir al carrito</button>
+              <a class="boton-base boton-secundario" href="#">Ver ediciones</a>
             </form>
           </div>
         </div>
@@ -49,152 +50,160 @@
     </section>
 
     <section class="section cover-img" aria-label="Segunda imagen">
-      <img src="img/<?php echo $game_data['featured_image']; ?>" alt="Imagen destacada del juego" /> 
+      <img src="img/<?php echo $game_data['featured_image']; ?>" alt="Imagen destacada del juego" />
 
       <div class="content">
         <h2><?php echo $game_data['title']; ?></h2>
+
         <p><?php echo $game_data['description']; ?></p>
       </div>
     </section>
-    
-    </div>
-    <div id="botones">
-      <a href="games.php">
-        <button class="btn azul">Juego</button>
-      </a>
 
-      <a href="community.php">
-        <button class="btn azul">Comunidad</button>
-      </a>
-    </div>
-    <section class="section cta" aria-label="Sección final - llamada a la acción">
-      <section class="section bottom-feed" aria-label="Feed y recomendaciones">
-        <div class="bottom-grid">
+  </div>
+  <div id="botones">
+    <a href="games.php">
+      <button class="btn azul">Juego</button>
+    </a>
+
+    <a href="community.php">
+      <button class="btn azul">Comunidad</button>
+    </a>
+  </div>
+  <section class="section cta" aria-label="Sección final - llamada a la acción">
+    <section class="section bottom-feed" aria-label="Feed y recomendaciones">
+      <div class="bottom-grid">
 
 
-          <aside class="col share">
-            <h4>Share</h4>
+        <aside class="col share">
+          <h4>Share</h4>
 
-            <label class="share-input">
-              <span class="sr-only">Enlace</span>
-              <input type="text" placeholder="Enlace" value="#" aria-label="Enlace a compartir">
-            </label>
+          <label class="share-input">
+            <span class="sr-only">Enlace</span>
+            <input type="text" placeholder="Enlace" value="#" aria-label="Enlace a compartir">
+          </label>
 
-            <div class="social-row">
-              <a class="boton-base social-btn twitter" href="https://twitter.com/intent/tweet?url=" target="_blank" rel="noopener noreferrer">Twitter</a>
-              <a class="boton-base social-btn facebook" href="https://www.facebook.com/sharer/sharer.php?u=" target="_blank" rel="noopener noreferrer">Facebook</a>
+          <div class="social-row">
+            <a class="boton-base social-btn twitter" href="https://twitter.com/intent/tweet?url=" target="_blank" rel="noopener noreferrer">Twitter</a>
+            <a class="boton-base social-btn facebook" href="https://www.facebook.com/sharer/sharer.php?u=" target="_blank" rel="noopener noreferrer">Facebook</a>
+          </div>
+        </aside>
+
+
+        <main class="col posts">
+
+          <a class="publicacion" href="communityPublication.php">
+            <div id="imgUsComunidad">
+              <img src="img/" alt="Imagen de perfil"><!--Aca deberia estar el perfil del usuario q hizo la publicación-->
+
+              <p>@Usuario</p>
             </div>
-          </aside>
+
+            <div class="contenido">
+              <p>Lorem Ipsum is simply dummy text of the printingLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the </p>
+
+              <img class="imgPub" src="" alt="">
+
+              <div class="interacciones">
+                <p>
+                  <i class="bi bi-hand-thumbs-up"></i> 0 <!--Este número es la cantidad de likes-->
+                </p>
+
+                <p>
+                  <i class="bi bi-hand-thumbs-down"></i> 0 <!--Este número es la cantidad de dislikes-->
+                </p>
+
+                <p>
+                  <i class="bi bi-chat"></i> 0 <!--Este número es la cantidad de comentarios-->
+                </p>
+              </div>
+            </div>
+          </a>
 
 
-          <main class="col posts">
 
-           <a class="publicacion" href="communityPublication.php">
-                <div id="imgUsComunidad">
-                    <img src="img/" alt="Imagen de perfil"><!--Aca deberia estar el perfil del usuario q hizo la publicación-->
+          <a class="publicacion" href="communityPublication.php">
+            <div id="imgUsComunidad">
+              <img src="img/" alt="Imagen de perfil"><!--Aca deberia estar el perfil del usuario q hizo la publicación-->
 
-                    <p>@Usuario</p>
-                </div>
+              <p>@Usuario</p>
+            </div>
 
-                <div class="contenido">
-                    <p>Lorem Ipsum is simply dummy text of the printingLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the </p>
+            <div class="contenido">
+              <p>Lorem Ipsum is simply dummy text of the printingLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the </p>
 
-                    <img class="imgPub" src="" alt="">
+              <img class="imgPub" src="" alt="">
 
-                    <div class="interacciones">
-                        <p>
-                            <i class="bi bi-hand-thumbs-up"></i> 0 <!--Este número es la cantidad de likes-->
-                        </p>
+              <div class="interacciones">
+                <p>
+                  <i class="bi bi-hand-thumbs-up"></i> 0 <!--Este número es la cantidad de likes-->
+                </p>
 
-                        <p>
-                            <i class="bi bi-hand-thumbs-down"></i> 0 <!--Este número es la cantidad de dislikes-->
-                        </p>
+                <p>
+                  <i class="bi bi-hand-thumbs-down"></i> 0 <!--Este número es la cantidad de dislikes-->
+                </p>
 
-                        <p>
-                            <i class="bi bi-chat"></i> 0 <!--Este número es la cantidad de comentarios-->
-                        </p>
-                    </div>
-                </div>
+                <p>
+                  <i class="bi bi-chat"></i> 0 <!--Este número es la cantidad de comentarios-->
+                </p>
+              </div>
+            </div>
+          </a>
+
+
+
+          <div class="more-wrap">
+            <a class="boton-base more-btn" href="#">Más Información</a>
+          </div>
+        </main>
+
+
+        <aside class="col recommended">
+          <h4>Recomendados</h4>
+
+          <div class="rec-item">
+            <a class="rec-thumb" href="#">
+              <img src="#" alt="rec 1">
             </a>
 
+            <div class="rec-meta">
+              <div class="rec-title">Titulo del juego recomendado </div>
 
+              <div class="rec-sub">Desarrollador del juego</div>
+            </div>
+          </div>
 
-           <a class="publicacion" href="communityPublication.php">
-                <div id="imgUsComunidad">
-                    <img src="img/" alt="Imagen de perfil"><!--Aca deberia estar el perfil del usuario q hizo la publicación-->
-
-                    <p>@Usuario</p>
-                </div>
-
-                <div class="contenido">
-                    <p>Lorem Ipsum is simply dummy text of the printingLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the </p>
-
-                    <img class="imgPub" src="" alt="">
-
-                    <div class="interacciones">
-                        <p>
-                            <i class="bi bi-hand-thumbs-up"></i> 0 <!--Este número es la cantidad de likes-->
-                        </p>
-
-                        <p>
-                            <i class="bi bi-hand-thumbs-down"></i> 0 <!--Este número es la cantidad de dislikes-->
-                        </p>
-
-                        <p>
-                            <i class="bi bi-chat"></i> 0 <!--Este número es la cantidad de comentarios-->
-                        </p>
-                    </div>
-                </div>
+          <div class="rec-item">
+            <a class="rec-thumb" href="#">
+              <img src="#" alt="rec 2">
             </a>
 
+            <div class="rec-meta">
+              <div class="rec-title">Titulo del juego recomendado</div>
 
-
-            <div class="more-wrap">
-              <a class="boton-base more-btn" href="#">Más Información</a>
+              <div class="rec-sub">Desarrollador del juego</div>
             </div>
-          </main>
+          </div>
 
+          <div class="rec-item">
+            <a class="rec-thumb" href="#">
+              <img src="#" alt="rec 3">
+            </a>
 
-          <aside class="col recommended">
-            <h4>Recomendados</h4>
+            <div class="rec-meta">
+              <div class="rec-title">Titulo del juego recomendado</div>
 
-            <div class="rec-item">
-              <a class="rec-thumb" href="#">
-                <img src="#" alt="rec 1">
-              </a>
-
-              <div class="rec-meta">
-                <div class="rec-title">Titulo del juego recomendado </div>
-                <div class="rec-sub">Desarrollador del juego</div>
-              </div>
+              <div class="rec-sub">Desarrollador del juego</div>
             </div>
-
-            <div class="rec-item">
-              <a class="rec-thumb" href="#">
-                <img src="#" alt="rec 2">
-              </a>
-
-              <div class="rec-meta">
-                <div class="rec-title">Titulo del juego recomendado</div>
-                <div class="rec-sub">Desarrollador del juego</div>
-              </div>
-            </div>
-
-            <div class="rec-item">
-              <a class="rec-thumb" href="#">
-                <img src="#" alt="rec 3">
-              </a>
-
-              <div class="rec-meta">
-                <div class="rec-title">Titulo del juego recomendado</div>
-                <div class="rec-sub">Desarrollador del juego</div>
-              </div>
-            </div>
-          </aside>
-        </div>
-      </section>
+          </div>
+        </aside>
+      </div>
 
     </section>
+
+  </section>
+
+  
+  
 </body>
 
 </html>
