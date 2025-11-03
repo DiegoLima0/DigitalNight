28a39c2d16c70e439f6b90b6fc511a23c541ffe4
DiegoLima0 <diegoalanlima98@gmail.com>
2025-11-03 17:08:19 -0300

    añado funcion de recomendados y todos los juegos a library | añado detalles al no tener ningun juego

--- library_processor.php
+++ library_processor.php
@@ -10,43 +10,108 @@ $idUser = $_SESSION['user_id'];
 
 $games_list = [];
 
-$sql_select_games = "SELECT idGame FROM user_game WHERE idUser=$idUser ORDER BY purchaseDate DESC";
+$sql_select_games = "
+  SELECT game.idGame, game.title, game.price, game.imagen
+  FROM user_game 
+  JOIN game ON user_game.idGame = game.idGame
+  WHERE user_game.idUser = $idUser
+  ORDER BY user_game.purchaseDate DESC
+";
 
 $result_games = $conexion->query($sql_select_games);
 
 if ($result_games && $result_games->num_rows > 0) {
-    while ($row = $result_games->fetch_assoc()) {
-        $idGame = $row['idGame'];
-
-        // Obtener los datos completos del juego
-        $sql_game_detail = "SELECT idGame, title, price, imagen FROM game WHERE idGame = $idGame";
-        $result_detail = $conexion->query($sql_game_detail);
-
-        if ($result_detail && $result_detail->num_rows > 0) {
-            $game_data = $result_detail->fetch_assoc();
-            $games_list[] = $game_data;
-        }
+    while ($game_data = $result_games->fetch_assoc()) {
+        $games_list[] = $game_data;
     }
 }
 
+
 $recent_games_list = [];
 
-$sql_select_recent_games = "SELECT idGame FROM user_game WHERE idUser=$idUser ORDER BY purchaseDate DESC LIMIT 2";
+$sql_select_recent_games = "
+  SELECT game.idGame, game.title, game.price, game.imagen
+  FROM user_game
+  JOIN game ON user_game.idGame = game.idGame
+  WHERE user_game.idUser = $idUser
+  ORDER BY user_game.purchaseDate DESC
+  LIMIT 2;
+";
 
 $result_recent_games = $conexion->query($sql_select_recent_games);
 
 if ($result_recent_games && $result_recent_games->num_rows > 0) {
-    while ($row = $result_recent_games->fetch_assoc()) {
-        $idGame = $row['idGame'];
+    while ($game_data = $result_recent_games->fetch_assoc()) {
+        $recent_games_list[] = $game_data;
+    }
+}
 
-        // Obtener los datos completos de los juegos recientes
-        $sql_game_detail = "SELECT idGame, title, price, imagen FROM game WHERE idGame = $idGame";
-        $result_detail = $conexion->query($sql_game_detail);
 
-        if ($result_detail && $result_detail->num_rows > 0) {
-            $game_data = $result_detail->fetch_assoc();
-            $recent_games_list[] = $game_data;
-        }
+$recommended_games = [];
+
+$sql_recommended = "
+  SELECT game.idGame, game.title, game.price, game.imagen
+  FROM game
+  WHERE genre = (
+    SELECT game.genre
+    FROM user_game
+    JOIN game ON user_game.idGame = game.idGame
+    WHERE user_game.idUser = $idUser
+    ORDER BY user_game.purchaseDate DESC
+    LIMIT 1
+  )
+  AND idGame NOT IN (
+    SELECT idGame
+    FROM user_game
+    WHERE idUser = $idUser
+  ) LIMIT 3
+";
+
+$result_recommended = $conexion->query($sql_recommended);
+
+if ($result_recommended && $result_recommended->num_rows > 0) {
+    while ($game = $result_recommended->fetch_assoc()) {
+        $recommended_games[] = $game;
+    }
+}
+
+
+$other_games = [];
+
+if (count($games_list) > 0) {
+    // El usuario tiene al menos un juego comprado
+    $sql_other_games = "
+      SELECT game.idGame, game.title, game.price, game.imagen
+      FROM game
+      WHERE genre != (
+        SELECT game.genre
+        FROM user_game
+        JOIN game ON user_game.idGame = game.idGame
+        WHERE user_game.idUser = $idUser
+        ORDER BY user_game.purchaseDate DESC
+        LIMIT 1
+      )
+      AND idGame NOT IN (
+        SELECT idGame
+        FROM user_game
+        WHERE idUser = $idUser
+      )
+    ";
+    $borro="";
+} else {
+    // El usuario no tiene juegos comprados: mostrar todos los juegos
+    $sql_other_games = "
+      SELECT idGame, title, price, imagen
+      FROM game
+    ";
+    $borro="borrar";
+}
+
+$result_other_games = $conexion->query($sql_other_games);
+
+if ($result_other_games && $result_other_games->num_rows > 0) {
+    while ($game = $result_other_games->fetch_assoc()) {
+        $other_games[] = $game;
     }
 }
 ?>
--- views/library_view.php
+++ views/library_view.php
@@ -28,7 +28,7 @@
           ?>
           <p>No hay juegos comprados.</p>
           <?php endif; ?>
-          <form method="POST" action="delete_games.php">
+          <form method="POST" action="delete_games.php" class="<?php echo $borro; ?>">
             <input type="hidden" name="idGame" value="<?php echo $game_data['idGame']; ?>">
             <button type="submit" class="boton-base boton-primario">borrar juegos</button>
           </form>
@@ -38,7 +38,7 @@
 
       <main class="contenido-juegos">
         <section class="seccion-juegos">
-          <h3>Agregados recientemente</h3>
+          <h3 class="<?php echo $borro; ?>">Agregados recientemente</h3>
 
           <div class="rejilla-juegos">
             <?php 
@@ -58,24 +58,22 @@
         </section>
 
         <section class="seccion-juegos">
-          <h3>Recomendados</h3>
+          <h3 class="<?php echo $borro; ?>">Recomendados</h3>
 
           <div class="rejilla-juegos">
-            <a href="juego5.html" class="recuadro-juego">
-              <img src="img/fallou4-recomendado.jpg" alt="Juego 3">
+            <?php 
+            if (isset($recommended_games) && is_array($recommended_games)): 
+              foreach ($recommended_games as $recommended_games): 
+            ?>
+            <a href="games.php?idGame=<?php echo $recommended_games['idGame']; ?>" class="recuadro-juego">
+              <img src="img/<?php echo $recommended_games['imagen']; ?>" alt="Juego">
             </a>
-
-            <a href="juego6.html" class="recuadro-juego">
-              <img src="img/doom.jpg" alt="Juego 3">
-            </a>
-
-            <a href="juego7.html" class="recuadro-juego">
-              <img src="img/peak.jpg" alt="Juego 3">
-            </a>
-            
-            <a href="juego8.html" class="recuadro-juego">
-              <img src="img/luto.jpg" alt="Juego 3">
-            </a>
+            <?php 
+              endforeach;
+              else:
+            ?>
+            <p>No hay juegos recomendados.</p>
+            <?php endif; ?>
           </div>
         </section>
 
@@ -83,53 +81,19 @@
           <h3>Todos los juegos</h3>
           
           <div class="rejilla-juegos">
-            <a href="juego9.html" class="recuadro-juego">
-              <img src="img/overcooked2.jpg" alt="Juego 3">
+            <?php 
+            if (isset($other_games) && is_array($other_games)): 
+              foreach ($other_games as $other_games): 
+            ?>
+            <a href="games.php?idGame=<?php echo $other_games['idGame']; ?>" class="recuadro-juego">
+              <img src="img/<?php echo $other_games['imagen']; ?>" alt="Juego">
             </a>
-
-            <a href="juego10.html" class="recuadro-juego">
-              <img src="img/gtav.jpg" alt="Juego 3">
-            </a>
-
-            <a href="juego11.html" class="recuadro-juego">
-              <img src="img/repo.jpg" alt="Juego 3">
-            </a>
-
-            <a href="juego12.html" class="recuadro-juego">
-              <img src="img/schedule.jpg" alt="Juego 3">
-            </a>
-
-            <a href="juego13.html" class="recuadro-juego">
-              <img src="img/metalgear.jpg" alt="Juego 3">
-            </a>
-
-            <a href="juego14.html" class="recuadro-juego">
-              <img src="img/little.jpg" alt="Juego 3">
-            </a>
-
-            <a href="juego15.html" class="recuadro-juego">
-              <img src="img/sons of the forest.jpg" alt="Juego 3">
-            </a>
-
-            <a href="juego16.html" class="recuadro-juego">
-              <img src="img/peak.jpg" alt="Juego 3">
-            </a>
-
-            <a href="juego17.html" class="recuadro-juego">
-              <img src="img/mafia2.jpg" alt="Juego 3">
-            </a>
-
-            <a href="juego18.html" class="recuadro-juego">
-              <img src="img/luto.jpg" alt="Juego 3">
-            </a>
-
-            <a href="juego19.html" class="recuadro-juego">
-              <img src="img/doom.jpg" alt="Juego 3">
-            </a>
-            
-            <a href="juego19.html" class="recuadro-juego">
-              <img src="img/overcooked2.jpg" alt="Juego 3">
-            </a>
+            <?php 
+              endforeach;
+              else:
+            ?>
+            <p>No hay otros juegos.</p>
+            <?php endif; ?>
           </div>
         </section>
       </main>
