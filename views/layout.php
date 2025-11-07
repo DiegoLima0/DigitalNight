03ae02351d0e2e914ff70387501d3235415e2728
DiegoLima0 <diegoalanlima98@gmail.com>
2025-11-07 19:34:33 -0300

    arreglo el icono

--- views/layout.php
+++ views/layout.php
@@ -2,11 +2,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/styles.css" rel="stylesheet">
-    <link rel="icon" href="img/digitalNightLogo.png">
     <script src="js/script.js" defer></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
 </head>
-
+<script>
+  const link = document.createElement('link');
+  link.rel = 'icon';
+  link.href = '/digitalNight/img/digitalNightLogo.png';
+  document.head.appendChild(link);
+</script>
 
 <body>
     <header>
